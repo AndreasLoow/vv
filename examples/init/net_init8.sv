863113ddfa9ad41b00192ba18143820d79430bbf
Andreas Lööw <AndreasLoow@users.noreply.github.com>
2023-06-20 00:47:50 +0100

    example of short-hand for wire + cont. assignment

---
+++ examples/init/net_init8.sv
@@ -0,0 +1,12 @@
+module net_init8;
+
+// Alternative short-hand notation for a wire declaration
+// plus a continuous assignment (see normalised Verilog module):
+wire #5 a = 0;
+
+// We can still add other continuous assignments 
+assign a = 1'bz;
+
+initial $monitor("%b at time = %d", a, $time);
+
+endmodule
