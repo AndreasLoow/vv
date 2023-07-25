97ca6c849b4a8e5d0632dd888876e0a186ae1bb5
Andreas Lööw <AndreasLoow@users.noreply.github.com>
2023-07-25 14:26:02 +0200

    extract meredith intro to separate module

--- examples/xx_more/Meredith/fig5_netassign1.sv
+++ examples/xx_more/Meredith/fig5_netassign1.sv
@@ -1,23 +1,7 @@
 module fig5_netassign1;
 
-// Meredith et al., in the conclusion of their
-// MEMOCODE'10 paper "A Formal Executable Semantics
-// of Verilog", says the following:
-//
-// "We believe that our definition can be useful
-// [...] as a starting point for discussion on
-// exactly what the standard should entail [...]."
-//
-// In the modules in this directory, we respond to
-// the questions and suggestions raised by Meredith et al..
-// We refer the tech. report version of the paper since
-// that version is slightly longer than the published
-// paper (12 pages vs. 10 pages).
-
-// We now address their discussion on Fig. 5:
-
-// Meredith et al. states that the following has
-// unclear semantics:
+// Meredith et al. states that the following
+// Verilog code has unclear semantics:
 
 wire w;
 reg r;
---
+++ examples/xx_more/Meredith/readme.sv
@@ -0,0 +1,17 @@
+module readme;
+
+// Meredith et al., in the conclusion of their
+// MEMOCODE'10 paper "A Formal Executable Semantics
+// of Verilog", says the following:
+//
+// "We believe that our definition can be useful
+// [...] as a starting point for discussion on
+// exactly what the standard should entail [...]."
+//
+// In the modules in this directory, we respond to
+// the questions and suggestions raised by Meredith et al..
+// We refer the tech. report version of the paper since
+// that version is slightly longer than the published
+// paper (12 pages vs. 10 pages).
+
+endmodule
