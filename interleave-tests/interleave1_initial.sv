bdcce33498d3aedf23e2f7c36cfaba54746dbe93
Andreas Lööw <AndreasLoow@users.noreply.github.com>
2023-09-18 00:20:28 +0100

    also test interleavings of initial processes

--- interleave-tests/interleave1_initial.sv
+++ interleave-tests/interleave1_initial.sv
@@ -4,28 +4,28 @@ module interleave1_initial;
 
 Aldec Riviera:
 
-before: a = x, b = xxxxxxxx, b(arr) = xxxxxxxx, c = xxxx, c(arr) = xxxx
-after:  a = 1, b = x1xxxxxx, b(arr) = x1xxxxxx, c = xxxx, c(arr) = xxxx
+before: i = x, v = xxxxxxxxxx, v(arr) = xxxxxxxxxx, n = xxxx, n(arr) = xxxx
+after:  i = 1, v = x1xxxxxxxx, v(arr) = x1xxxxxxxx, n = xxxx, n(arr) = xxxx
 
 Cadence Xcelium:
 
-before: a = x, b = xxxxxxxx, b(arr) = xxxxxxxx, c = xxxx, c(arr) = xxxx
-after:  a = 1, b = xxxxxxxx, b(arr) = xxxxxxxx, c = xxxx, c(arr) = xxxx
+before: i = x, v = xxxxxxxxxx, v(arr) = xxxxxxxxxx, n = xxxx, n(arr) = xxxx
+after:  i = 1, v = xxxxxxxxxx, v(arr) = xxxxxxxxxx, n = xxxx, n(arr) = xxxx
 
 Mentor Questa:
 
-before: a = x, b = xxxxxxxx, b(arr) = xxxxxxxx, c = xxxx, c(arr) = xxxx
-after:  a = 1, b = xxxxxxxx, b(arr) = xxxxxxxx, c = xxxx, c(arr) = xxxx
+before: i = x, v = xxxxxxxxxx, v(arr) = xxxxxxxxxx, n = xxxx, n(arr) = xxxx
+after:  i = 1, v = xxxxxxxxxx, v(arr) = xxxxxxxxxx, n = xxxx, n(arr) = xxxx
 
 Synopsys VCS:
 
-before: a = x, b = xxxxxxxx, b(arr) = xxxxxxxx, c = xxxx, c(arr) = xxxx
-after:  a = 1, b = 01xxx101, b(arr) = 01xxxx01, c = 0101, c(arr) = 0101
+before: i = x, v = xxxxxxxxxx, v(arr) = xxxxxxxxxx, n = xxxx, n(arr) = xxxx
+after:  i = 1, v = 01xxx1xx01, v(arr) = 01xxxxxx01, n = 0101, n(arr) = 0101
 
 Icarus:
 
-before: a = x, b = xxxxxxxx, b(arr) = xxzzzzxx, c = xxxx, c(arr) = xxxx
-after:  a = 1, b = xxxxxxx1, b(arr) = xxzzzzx1, c = xxx1, c(arr) = xxx1
+before: i = x, v = xxxxxxxxxx, v(arr) = xxzzzzzzxx, n = xxxx, n(arr) = xxxx
+after:  i = 1, v = xxxxxxxxx1, v(arr) = xxzzzzzzx1, n = xxx1, n(arr) = xxx1
 
 */
 
@@ -38,71 +38,80 @@ Riviera only interleaves one case: buf gate, but only for variables, not for net
 Xcelium and Questa do not interleave anything.
 
 Synopsys interleaves all continuous assignments and gate assignments (for both variables and nets).
-Interestingly, the simple always@(*) assignment (but not the always_comb assignment) is interleaved
-in the scalar case but not the array case...
+Interestingly, the simple always@(*) assignment (but not the initial or always_comb assignments)
+is interleaved in the scalar case but not the array case...
 
 Icarus only interleaves the simple continuous assignment (for both variables and nets).
 
 */
 
-logic a;
+// "i" for input
+logic i;
 
-logic b7, b6, b5, b4, b3, b2, b1, b0;
-logic[7:0] b;
+// "v" for variable
+logic v9, v8, v7, v6, v5, v4, v3, v2, v1, v0;
+logic[9:0] v;
 
-wire c3, c2, c1, c0;
-wire[3:0] c;
+// "n" for net
+wire n3, n2, n1, n0;
+wire[3:0] n;
 
 initial begin
- $display("before: a = %b, b = %b, b(arr) = %b, c = %b, c(arr) = %b",
-	  a, {b7,b6,b5,b4,b3,b2,b1,b0}, b, {c3,c2,c1,c0}, c);
- a = 1;
- $display("after:  a = %b, b = %b, b(arr) = %b, c = %b, c(arr) = %b",
-	  a, {b7,b6,b5,b4,b3,b2,b1,b0}, b, {c3,c2,c1,c0}, c);
+ $display("before: i = %b, v = %b, v(arr) = %b, n = %b, n(arr) = %b",
+	  i, {v9,v8,v7,v6,v5,v4,v3,v2,v1,v0}, v, {n3,n2,n1,n0}, n);
+ i = 1;
+ $display("after:  i = %b, v = %b, v(arr) = %b, n = %b, n(arr) = %b",
+	  i, {v9,v8,v7,v6,v5,v4,v3,v2,v1,v0}, v, {n3,n2,n1,n0}, n);
 end
 
 // Assignments to variable
 
-assign b0 = a;
-assign b1 = a + 1;
+assign v0 = i;
+assign v1 = i + 1;
 
-always @(*) b2 = a;
-always @(*) b3 = a + 1;
+initial @(*) v2 = i;
+initial @(*) v3 = i + 1;
 
-always_comb b4 = a;
-always_comb b5 = a + 1;
+always @(*) v4 = i;
+always @(*) v5 = i + 1;
 
-buf (b6, a);
-not (b7, a);
+always_comb v6 = i;
+always_comb v7 = i + 1;
+
+buf (v8, i);
+not (v9, i);
 
 // Assignments to variable (array)
 
-assign b[0] = a;
-assign b[1] = a + 1;
+assign v[0] = i;
+assign v[1] = i + 1;
 
-always @(*) b[2] = a;
-always @(*) b[3] = a + 1;
+initial @(*) v[2] = i;
+initial @(*) v[3] = i + 1;
 
-always_comb b[4] = a;
-always_comb b[5] = a + 1;
+always @(*) v[4] = i;
+always @(*) v[5] = i + 1;
 
-buf (b[6], a);
-not (b[7], a);
+always_comb v[6] = i;
+always_comb v[7] = i + 1;
+
+buf (v[8], i);
+not (v[9], i);
 
 // Assignments to net
 
-assign c0 = a;
-assign c1 = a + 1;
+assign n0 = i;
+assign n1 = i + 1;
 
-buf (c2, a);
-not (c3, a);
+buf (n2, i);
+not (n3, i);
 
 // Assignments to net (array)
 
-assign c[0] = a;
-assign c[1] = a + 1;
+assign n[0] = i;
+assign n[1] = i + 1;
 
-buf (c[2], a);
-not (c[3], a);
+buf (n[2], i);
+not (n[3], i);
 
 endmodule
--- interleave-tests/interleave2_always_comb.sv
+++ interleave-tests/interleave2_always_comb.sv
@@ -4,46 +4,46 @@ module interleave2_always_comb;
 
 Aldec Riviera:
 
-before: a = x, b = xxxxxxxx, b(arr) = xxxxxxxx, c = xxxx, c(arr) = xxxx
-after:  a = 1, b = x1xxxxxx, b(arr) = x1xxxxxx, c = xxxx, c(arr) = xxxx
-before: a = 1, b = 01010101, b(arr) = 01010101, c = 0101, c(arr) = 0101
-after:  a = 1, b = 01010101, b(arr) = 01010101, c = 0101, c(arr) = 0101
+before: i = x, v = xxxxxxxxxx, v(arr) = xxxxxxxxxx, n = xxxx, n(arr) = xxxx
+after:  i = 1, v = x1xxxxxxxx, v(arr) = x1xxxxxxxx, n = xxxx, n(arr) = xxxx
+before: i = 1, v = 0101010101, v(arr) = 0101010101, n = 0101, n(arr) = 0101
+after:  i = 1, v = 0101010101, v(arr) = 0101010101, n = 0101, n(arr) = 0101
 
 Cadence Xcelium:
 
-before: a = x, b = xxxxxxxx, b(arr) = xxxxxxxx, c = xxxx, c(arr) = xxxx
-after:  a = 1, b = xxxxxxxx, b(arr) = xxxxxxxx, c = xxxx, c(arr) = xxxx
-before: a = 1, b = xx010101, b(arr) = xx010101, c = xxxx, c(arr) = xxxx
-after:  a = 1, b = xx010101, b(arr) = xx010101, c = xxxx, c(arr) = xxxx
-before: a = 1, b = xx010101, b(arr) = xx010101, c = 0101, c(arr) = 0101
-after:  a = 1, b = xx010101, b(arr) = xx010101, c = 0101, c(arr) = 0101
-before: a = 1, b = 01010101, b(arr) = 01010101, c = 0101, c(arr) = 0101
-after:  a = 1, b = 01010101, b(arr) = 01010101, c = 0101, c(arr) = 0101
+before: i = x, v = xxxxxxxxxx, v(arr) = xxxxxxxxxx, n = xxxx, n(arr) = xxxx
+after:  i = 1, v = xxxxxxxxxx, v(arr) = xxxxxxxxxx, n = xxxx, n(arr) = xxxx
+before: i = 1, v = xx01010101, v(arr) = xx01010101, n = xxxx, n(arr) = xxxx
+after:  i = 1, v = xx01010101, v(arr) = xx01010101, n = xxxx, n(arr) = xxxx
+before: i = 1, v = xx01010101, v(arr) = xx01010101, n = 0101, n(arr) = 0101
+after:  i = 1, v = xx01010101, v(arr) = xx01010101, n = 0101, n(arr) = 0101
+before: i = 1, v = 0101010101, v(arr) = 0101010101, n = 0101, n(arr) = 0101
+after:  i = 1, v = 0101010101, v(arr) = 0101010101, n = 0101, n(arr) = 0101
 
 Mentor Questa:
 
-before: a = x, b = xxxxxxxx, b(arr) = xxxxxxxx, c = xxxx, c(arr) = xxxx
-after:  a = 1, b = xxxxxxxx, b(arr) = xxxxxxxx, c = xxxx, c(arr) = xxxx
-before: a = 1, b = 01xxxx01, b(arr) = 01xxxx01, c = 0101, c(arr) = 0101
-after:  a = 1, b = 01xxxx01, b(arr) = 01xxxx01, c = 0101, c(arr) = 0101
-before: a = 1, b = 01010101, b(arr) = 01010101, c = 0101, c(arr) = 0101
-after:  a = 1, b = 01010101, b(arr) = 01010101, c = 0101, c(arr) = 0101
+before: i = x, v = xxxxxxxxxx, v(arr) = xxxxxxxxxx, n = xxxx, n(arr) = xxxx
+after:  i = 1, v = xxxxxxxxxx, v(arr) = xxxxxxxxxx, n = xxxx, n(arr) = xxxx
+before: i = 1, v = 01xxxxxx01, v(arr) = 01xxxxxx01, n = 0101, n(arr) = 0101
+after:  i = 1, v = 01xxxxxx01, v(arr) = 01xxxxxx01, n = 0101, n(arr) = 0101
+before: i = 1, v = 0101010101, v(arr) = 0101010101, n = 0101, n(arr) = 0101
+after:  i = 1, v = 0101010101, v(arr) = 0101010101, n = 0101, n(arr) = 0101
 
 Synopsys VCS:
 
-before: a = x, b = xxxxxxxx, b(arr) = xxxxxxxx, c = xxxx, c(arr) = xxxx
-after:  a = 1, b = xxxxx1xx, b(arr) = xxxxxxxx, c = xxxx, c(arr) = xxxx
-before: a = 1, b = 0101x101, b(arr) = 0101xx01, c = 0101, c(arr) = xxxx
-after:  a = 1, b = 0101x101, b(arr) = 0101xx01, c = 0101, c(arr) = xxxx
-before: a = 1, b = 01010101, b(arr) = 01010101, c = 0101, c(arr) = 0101
-after:  a = 1, b = 01010101, b(arr) = 01010101, c = 0101, c(arr) = 0101
+before: i = x, v = xxxxxxxxxx, v(arr) = xxxxxxxxxx, n = xxxx, n(arr) = xxxx
+after:  i = 1, v = xxxxx1xxxx, v(arr) = xxxxxxxxxx, n = xxxx, n(arr) = xxxx
+before: i = 1, v = 0101x1xx01, v(arr) = 0101xxxx01, n = 0101, n(arr) = xxxx
+after:  i = 1, v = 0101x1xx01, v(arr) = 0101xxxx01, n = 0101, n(arr) = xxxx
+before: i = 1, v = 0101010101, v(arr) = 0101010101, n = 0101, n(arr) = 0101
+after:  i = 1, v = 0101010101, v(arr) = 0101010101, n = 0101, n(arr) = 0101
 
 Icarus:
 
-before: a = x, b = xxxxxxxx, b(arr) = xxxxzzxx, c = xxxx, c(arr) = xxxx
-after:  a = 1, b = xxxxxxx1, b(arr) = xxxxzzx1, c = xxx1, c(arr) = xxx1
-before: a = 1, b = 01010101, b(arr) = 01010101, c = 0101, c(arr) = 0101
-after:  a = 1, b = 01010101, b(arr) = 01010101, c = 0101, c(arr) = 0101
+before: i = x, v = xxxxxxxxxx, v(arr) = xxxxzzzzxx, n = xxxx, n(arr) = xxxx
+after:  i = 1, v = xxxxxxxxx1, v(arr) = xxxxzzzzx1, n = xxx1, n(arr) = xxx1
+before: i = 1, v = 0101010101, v(arr) = 0101010101, n = 0101, n(arr) = 0101
+after:  i = 1, v = 0101010101, v(arr) = 0101010101, n = 0101, n(arr) = 0101
 
 */
 
@@ -52,71 +52,77 @@ after:  a = 1, b = 01010101, b(arr) = 01010101, c = 0101, c(arr) = 0101
 Observations:
 
 Riviera, Xcelium, Questa and Icarus interleave the same things as in
-interleave1_initial (see first two rows from each simulator).
+interleave1_initial (see first two rows of output from each simulator).
 
 VCS, in contrast, interleaves less than in interleave1_initial!
-Only the the simple always@(*) assignment for the scalar case remains...
+Only the simple always@(*) assignment for the scalar case remains...
 
 */
 
-logic a;
+logic i;
 
-logic b7, b6, b5, b4, b3, b2, b1, b0;
-logic[7:0] b;
+logic v9, v8, v7, v6, v5, v4, v3, v2, v1, v0;
+logic[9:0] v;
 
-wire c3, c2, c1, c0;
-wire[3:0] c;
+wire n3, n2, n1, n0;
+wire[3:0] n;
 
 always_comb begin
- $display("before: a = %b, b = %b, b(arr) = %b, c = %b, c(arr) = %b",
-	  a, {b7,b6,b5,b4,b3,b2,b1,b0}, b, {c3,c2,c1,c0}, c);
- a = 1;
- $display("after:  a = %b, b = %b, b(arr) = %b, c = %b, c(arr) = %b",
-	  a, {b7,b6,b5,b4,b3,b2,b1,b0}, b, {c3,c2,c1,c0}, c);
+ $display("before: i = %b, v = %b, v(arr) = %b, n = %b, n(arr) = %b",
+	  i, {v9,v8,v7,v6,v5,v4,v3,v2,v1,v0}, v, {n3,n2,n1,n0}, n);
+ i = 1;
+ $display("after:  i = %b, v = %b, v(arr) = %b, n = %b, n(arr) = %b",
+	  i, {v9,v8,v7,v6,v5,v4,v3,v2,v1,v0}, v, {n3,n2,n1,n0}, n);
 end
 
 // Assignments to variable
 
-assign b0 = a;
-assign b1 = a + 1;
+assign v0 = i;
+assign v1 = i + 1;
 
-always @(*) b2 = a;
-always @(*) b3 = a + 1;
+initial @(*) v2 = i;
+initial @(*) v3 = i + 1;
 
-always_comb b4 = a;
-always_comb b5 = a + 1;
+always @(*) v4 = i;
+always @(*) v5 = i + 1;
 
-buf (b6, a);
-not (b7, a);
+always_comb v6 = i;
+always_comb v7 = i + 1;
+
+buf (v8, i);
+not (v9, i);
 
 // Assignments to variable (array)
 
-assign b[0] = a;
-assign b[1] = a + 1;
+assign v[0] = i;
+assign v[1] = i + 1;
 
-always @(*) b[2] = a;
-always @(*) b[3] = a + 1;
+initial @(*) v[2] = i;
+initial @(*) v[3] = i + 1;
 
-always_comb b[4] = a;
-always_comb b[5] = a + 1;
+always @(*) v[4] = i;
+always @(*) v[5] = i + 1;
 
-buf (b[6], a);
-not (b[7], a);
+always_comb v[6] = i;
+always_comb v[7] = i + 1;
+
+buf (v[8], i);
+not (v[9], i);
 
 // Assignments to net
 
-assign c0 = a;
-assign c1 = a + 1;
+assign n0 = i;
+assign n1 = i + 1;
 
-buf (c2, a);
-not (c3, a);
+buf (n2, i);
+not (n3, i);
 
 // Assignments to net (array)
 
-assign c[0] = a;
-assign c[1] = a + 1;
+assign n[0] = i;
+assign n[1] = i + 1;
 
-buf (c[2], a);
-not (c[3], a);
+buf (n[2], i);
+not (n[3], i);
 
 endmodule
